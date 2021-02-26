4fcc9cc9cb9d1f8c3b005dca42db995a41abbaf4
Panpan Zhou <pz7@rice.edu>
2021-02-25 22:06:22 -0600

    Add files via upload

---
+++ src/chatbot.h
@@ -0,0 +1,52 @@
+#ifndef CHATBOT_H_
+#define CHATBOT_H_
+
+#include <wx/bitmap.h>
+#include <string>
+#include <memory>
+
+class GraphNode; // forward declaration
+class ChatLogic; // forward declaration
+
+class ChatBot
+{
+private:
+    // data handles (owned)
+    wxBitmap *_image; // avatar image
+
+    // data handles (not owned)
+    GraphNode *_currentNode;
+    GraphNode *_rootNode;
+    ChatLogic *_chatLogic;
+
+    // proprietary functions
+    int ComputeLevenshteinDistance(std::string s1, std::string s2);
+
+public:
+    // constructors / destructors
+    ChatBot();                     // constructor WITHOUT memory allocation
+    ChatBot(std::string filename); // constructor WITH memory allocation
+    ~ChatBot();
+
+    //// STUDENT CODE
+    ////
+    ChatBot(const ChatBot & chatbotSource); // copy constructor
+    ChatBot(ChatBot && chatbotSource); // move constructor
+    ChatBot &operator = (const ChatBot & chatbotSource); //copy assignment constructor
+    ChatBot &operator = (ChatBot && chatbotSource); //move assignment constructor
+    
+    ChatLogic * GetChatLogicHandle() {return _chatLogic;}
+    ////
+    //// EOF STUDENT CODE
+
+    // getters / setters
+    void SetCurrentNode(GraphNode *node);
+    void SetRootNode(GraphNode *rootNode) { _rootNode = rootNode; }
+    void SetChatLogicHandle(ChatLogic *chatLogic) { _chatLogic = chatLogic; }
+    wxBitmap *GetImageHandle() { return _image; }
+
+    // communication
+    void ReceiveMessageFromUser(std::string message);
+};
+
+#endif /* CHATBOT_H_ */
---
+++ src/graphnode.cpp
@@ -0,0 +1,65 @@
+#include "graphedge.h"
+#include "graphnode.h"
+#include "chatlogic.h"
+#include <iostream>
+
+GraphNode::GraphNode(int id)
+{
+    std::cout << "GraphNode Constructor" << std::endl;
+    _id = id;
+}
+
+GraphNode::~GraphNode()
+{
+    //// STUDENT CODE
+    ////
+
+    //delete _chatBot; 
+    std::cout << "GraphNode Destructor" << std::endl;
+    ////
+    //// EOF STUDENT CODE
+}
+
+void GraphNode::AddToken(std::string token)
+{
+    _answers.push_back(token);
+}
+
+void GraphNode::AddEdgeToParentNode(GraphEdge *edge)
+{
+    _parentEdges.push_back(edge);
+}
+
+void GraphNode::AddEdgeToChildNode(GraphEdge *edge)
+{
+    _childEdges.push_back(std::unique_ptr<GraphEdge>(edge));
+}
+
+//// STUDENT CODE
+////
+void GraphNode::MoveChatbotHere(ChatBot &&chatBot)
+{
+    _chatBot = std::move(chatBot);
+    //_chatBot->SetCurrentNode(this);
+    _chatBot.GetChatLogicHandle()->SetChatbotHandle(&_chatBot);
+    _chatBot.SetCurrentNode(this);
+}
+
+void GraphNode::MoveChatbotToNewNode(GraphNode *newNode)
+{
+    newNode->MoveChatbotHere(std::move(_chatBot));
+    //_chatBot = nullptr; // invalidate pointer at source
+}
+////
+//// EOF STUDENT CODE
+
+GraphEdge *GraphNode::GetChildEdgeAtIndex(int index)
+{
+    //// STUDENT CODE
+    ////
+
+    return _childEdges[index].get();
+
+    ////
+    //// EOF STUDENT CODE
+}
